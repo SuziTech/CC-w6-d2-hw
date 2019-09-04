d88cd818d7e2cb4e7267c6c421ffd9cc74422171
Suzanne Dyson <suzanne.dyson2334@gmail.com>
2019-09-04 13:39:55 +0100

    park changes

--- models/park.js
+++ models/park.js
@@ -4,4 +4,11 @@ const Park = function (name, ticketPrice, dinosaurCollection) {
   this.dinosaurCollection = dinosaurCollection;
 }
 
+Park.prototype.dinosaurCollectionLength = function () {
+  this.dinosaurCollection = [];
+  this.dinosaurCollection.push(dinosaur);
+  numberInDinosaurCollection = this.dinosaurCollection.length();
+  return numberInDinosaurCollection;
+}
+
 module.exports = Park;
